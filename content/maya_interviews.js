6686e1326a0c5a9b1e8222e900125bf91dc0deb7
Sarah German <sgerman@smm.org>
2013-03-21 15:08:57 -0500

    Initial file structure setup

---
+++ content/maya_interviews.js
@@ -0,0 +1,36 @@
+/*
+ * This array is passed to the page template, ../templates/video.handlebars,
+ * which renders it into the appropriate HTML elements.
+*/
+var data = {
+  PageTitle : "",
+  PageTitleSpanish : "",
+  Videos : [
+    {
+      Prompt : "",
+      SpanishPrompt: "",
+      File : "",
+      Image : ""
+    },
+    {
+      Prompt : "",
+      SpanishPrompt: "",
+      File : "",
+      Image : ""
+    },
+    {
+      Prompt : "",
+      SpanishPrompt: "",
+      File : "",
+      Image : ""
+    },
+    {
+      Prompt : "",
+      SpanishPrompt: "",
+      File : "",
+      Image : ""
+    }
+  ],
+  Back : "Back",
+  SpanishBack : "Atrasv"
+};
