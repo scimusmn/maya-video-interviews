7959c2165ad952cf748b85eee740824b4fb2702d
Sarah German <sgerman@smm.org>
2013-03-21 15:18:06 -0500

    Added content from Flash file

--- content/maya_interviews.js
+++ content/maya_interviews.js
@@ -3,32 +3,32 @@
  * which renders it into the appropriate HTML elements.
 */
 var data = {
-  PageTitle : "",
-  PageTitleSpanish : "",
+  PageTitle : "There are many reasons to investigate and preserve ancient Maya sites",
+  PageTitleSpanish : "Hay muchos motivos de investigar y conservar sitios mayas antiguos",
   Videos : [
     {
-      Prompt : "",
-      SpanishPrompt: "",
-      File : "",
-      Image : ""
+      Prompt : "How do lasers help you find undiscovered sites?",
+      SpanishPrompt: "¿Cómo rayos láser ayudarle a encontrar sitios por descubrir?",
+      File : "1115_Chip_012.mp4",
+      Image : "video1.png"
     },
     {
-      Prompt : "",
-      SpanishPrompt: "",
-      File : "",
-      Image : ""
+      Prompt : "What was a Maya city like when people still lived there?",
+      SpanishPrompt: "Lo que era una ciudad Maya como cuando las personas viven aún?",
+      File : "1115_Chip_014.mp4",
+      Image : "video2.png"
     },
     {
-      Prompt : "",
-      SpanishPrompt: "",
-      File : "",
-      Image : ""
+      Prompt : "Why were Maya cities abandoned?",
+      SpanishPrompt: "¿Por qué fueron abandonadas las ciudades mayas?",
+      File : "1115_Chip_018.mp4",
+      Image : "video3.png"
     },
     {
-      Prompt : "",
-      SpanishPrompt: "",
-      File : "",
-      Image : ""
+      Prompt : "Why do we preserve and visit Maya sites today?",
+      SpanishPrompt: "¿Por qué conservar y visitar sitios mayas de hoy?",
+      File : "1115_Chip_022.mp4",
+      Image : "video4.png"
     }
   ],
   Back : "Back",
