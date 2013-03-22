ce84362bca9684d88ecc2ed75ae6bcfa61a8cb60
Sarah German <sgerman@smm.org>
2013-03-22 12:11:50 -0500

    Removed extra functions
    Discarded functions for poster images and showing the active video since
    those aren't used in this design

--- js/video_kiosk.js
+++ js/video_kiosk.js
@@ -2,16 +2,13 @@
  * Add-on functionality for video.js
  * Extra features for better playback on an exhibit kiosk.
 */
-
 function video_kiosk() {
 
   // Set some variables, we'll use these a bunch
-  var $videoOption = $('#video-options div'),
+  var $videoOption = $('.video-button'),
       videoPlayer = _V_('videoPlayer'),
       // Path is relative to the kiosk page
       videoPath = 'media/video/';
-
-  $videoOption.eq(0).addClass('selected'); // Select the first video initially
 
   // Initialize the videojs plugin.
   videoPlayer.ready(function() {
@@ -21,27 +18,17 @@ function video_kiosk() {
       $('.vjs-big-play-button').css('visibility','visible').show();
     });
 
-    // Reset poster image when video ends
-    this.addEvent("ended", function() {
-      this.posterImage.show();
-    });
-
     // Toggle main video by clicking on its title or poster image
     $videoOption.click(function() {
-      var id = $(this).attr('id').match(/\d+/),
-          video = $(this).attr('data-video-source'),
-          poster = $('img', this).attr('src'),
+      var video = $(this).attr('data-video-source');
 
-      // Change the video source and poster attributes
-      if (!$(this).hasClass('selected')) {
-        videoPlayer.src(videoPath + video);
-        $('.vjs-poster').attr('src', poster);
+      // Change the video source
+      videoPlayer.src(videoPath + video);
 
-        // Be kind, rewind
-        videoPlayer.currentTime("0").posterImage.show();
-        $('.vjs-big-play-button').css('visibility','visible').show();
+      // Be kind, rewind
+      videoPlayer.currentTime("0").posterImage.show();
+      $('.vjs-big-play-button').css('visibility','visible').show();
 
-      }
     });
 
   });
