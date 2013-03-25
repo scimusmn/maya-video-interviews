ac53740dee5a849aa49a5a3c4e84456d8b898c86
Sarah German <sgerman@smm.org>
2013-03-25 13:59:21 -0500

    Scaled back tests.
    Most of the functionality here is DOM manipulation, which isn't a good
    fit for qUnit. Going to test the config file and content file only.
    
    See
    http://stackoverflow.com/questions/1435038/testing-javascript-that-manipulates-the-dom

--- tests/test_video_kiosk.js
+++ tests/test_video_kiosk.js
@@ -1,19 +1,10 @@
 /**
- * Unit tests, using qUnit - http://qunitjs.com/
- *
- * @TODO - Write some tests:
- *   - Does clicking a button play a video?
- *   - Does clicking "Back" take you back? And stop/rewind the video?
- *   - Does the screensaver start after 3 minutes?
- *   - Does the page reload if you move the mouse when the screensaver's running?
-*/
-
-/**
- * Test: Check config file.
+ * Test: Check config file
  */
-test("config", function() {
+test("kiosk init", function() {
   expect(3);
 
+  // Check config file contents
   var url = '../config/config.json';
   stop();
   $.getJSON(url, function(data) {
@@ -26,32 +17,11 @@ test("config", function() {
 });
 
 /**
- * Test: Check if content has been loaded into the template.
+ * Test: Check for content
  */
-test("template load", function() {
+test("content", function() {
 
-  expect(3); // Expect 3 assertions
+ // expect();
 
-  // Check for empty h1, h2, h3, h4 and p tags
-  var content = $('h1, h2, h3, h4, p').text();
-  notEqual(content, '');
-
-  // Make sure image has src
-  var image_source = [];
-  $('img').each(function() {
-    var source = $(this).attr('src');
-    image_source.push(source);
-  });
-  var total_images = $('img').length();
-  equal(total_images, image_source.length);
-
-  // Make sure videos have srcs
-  var video_source = [];
-  $('video').each(function() {
-    var source = $(this).attr('src');
-    video_source.push(source);
-  });
-  var total_videos = $('video').length();
-  equal(total_videos, video_source.length);
 
 });
