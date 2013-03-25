198756be9047ed4de808727d93b7847094a7c72c
Sarah German <sgerman@smm.org>
2013-03-24 20:06:54 -0500

    Defined list of things to test

---
+++ tests/test_video_kiosk.js
@@ -0,0 +1,12 @@
+/**
+ * Unit tests, using qUnit - http://qunitjs.com/
+ *
+ * @TODO - Write some tests:
+ *   1. Does the content load into the template?
+ *   2. Does clicking a button play a video?
+ *   3. Does clicking "Back" take you back? And stop/rewind the video?
+ *   4. Does the screensaver start after 3 minutes?
+ *   5. Does the page reload if you move the mouse when the screensaver's running?
+*/
+
+
