01e1e5837eaa5164dcbca93f18451b755e2f75e9
Sarah German <sgerman@smm.org>
2013-03-24 20:50:08 -0500

    Wrote test to check if content exists

--- tests/test_video_kiosk.js
+++ tests/test_video_kiosk.js
@@ -2,20 +2,37 @@
  * Unit tests, using qUnit - http://qunitjs.com/
  *
  * @TODO - Write some tests:
- *   1. Does the content load into the template?
- *   2. Does clicking a button play a video?
- *   3. Does clicking "Back" take you back? And stop/rewind the video?
- *   4. Does the screensaver start after 3 minutes?
- *   5. Does the page reload if you move the mouse when the screensaver's running?
+ *   - Does clicking a button play a video?
+ *   - Does clicking "Back" take you back? And stop/rewind the video?
+ *   - Does the screensaver start after 3 minutes?
+ *   - Does the page reload if you move the mouse when the screensaver's running?
 */
 
 /**
  * Test: Check if content has been loaded into the template.
  */
 test("template load", function() {
+
+  // Check for empty h1, h2, h3, h4 and p tags
   var content = $('h1, h2, h3, h4, p').text();
   notEqual(content, '');
-  // Check for empty h1, h2, h3, h4 and p tagis
+
   // Make sure image has src
-  // Make sure video has src
+  var image_source = [];
+  $('img').each(function() {
+    var source = $(this).attr('src');
+    image_source.push(source);
+  });
+  var total_images = $('img').length();
+  equal(total_images, image_source.length);
+
+  // Make sure videos have srcs
+  var video_source = [];
+  $('video').each(function() {
+    var source = $(this).attr('src');
+    video_source.push(source);
+  });
+  var total_videos = $('video').length();
+  equal(total_videos, video_source.length);
+
 });
