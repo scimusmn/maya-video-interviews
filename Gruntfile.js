ec8ecfab429b1b1f8729ecaaf074e451bb3c091a
Sarah German <sgerman@smm.org>
2013-03-25 17:12:07 -0500

    Set up Grunt uglify to minify all JS

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+        kiosk_scripts: {
+          files: {
+            'js/maya_interviews.min.js': [
+              'libs/jquery-1.9.1.min.js',
+              'libs/handlebars.js',
+              'libs/video.min.js',
+              'src/js/init.js',
+              'src/js/video_kiosk.js',
+              'src/content/maya_interviews.js',
+              'src/js/screensaver.js'
+            ]
+          }
+        }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
