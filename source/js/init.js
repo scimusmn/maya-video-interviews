7bd53279922dec2dc01e3af2d64a40b3937721ed
Sarah German <sgerman@smm.org>
2013-03-26 17:44:40 -0500

    Check if templates are precompiled, render accordingly

--- source/js/init.js
+++ source/js/init.js
@@ -3,9 +3,11 @@
  * - Load and read the config file.
  * - Load precompiled handlebars template.
  * - Load video playback behaviors from video_kiosk.js.
+ *
+ * @param compile - BOOLEAN - Is the template precompiled?
+ *
  */
-
-var kiosk_init = function() {
+var kiosk_init = function(compile) {
 
   // Path to the config file
   var config = '../config/config.json';
@@ -16,15 +18,36 @@ var kiosk_init = function() {
     // Set a body class with the component name
     $('body').addClass(data.component_name);
 
-    // Load the component's content from maya_interviews.js
-    // Load the precompiled template, and output these into #container
-    var component = eval(data.component_name),
-        template = Handlebars.templates['video'],
-        html = template(component);
-    $('#container').html(html);
+    // Set the component name as a variable
+    var component = eval(data.component_name);
 
-    // Video behaviors
-    video_kiosk();
+    if (compile !== true) {
+      console.log('Dev mode');
+
+      // If the templates aren't compiled, run them through the compiler
+      $.ajax({
+        url: 'templates/video.handlebars',
+        cache: true,
+        success: function (source) {
+          var template = Handlebars.compile(source);
+          $('#container').html(template(component));
+          // Then initialize video functionality
+          video_kiosk();
+        }
+      });
+
+    } else {
+      // Precompiled template just needs to be output into the DOM
+      var template = Handlebars.templates['video'],
+          html = template(component);
+
+      // Output markup into container div, then start video functionality
+      $('#container').html(html, function() {
+        video_kiosk();
+      });
+
+    }
+
 
   });
 
