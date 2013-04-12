b54fbcd9ffded973d0c9b9a5649941c3a134b9e4
Sarah German <sgerman@smm.org>
2013-04-12 13:45:02 -0500

    Updated tests to reflect path and config changes

--- tests/test_video_kiosk.js
+++ tests/test_video_kiosk.js
@@ -2,14 +2,13 @@
  * Test: Check config file
  */
 test("kiosk init", function() {
-  expect(3);
+  expect(2);
 
   // Check config file contents
   var url = '../config/config.json';
   stop();
   $.getJSON(url, function(data) {
     ok(data, 'Data is returned from the server'); // Data exists
-    ok(data.color, 'Color: ' + data.color); // Color parameter exists
     ok(data.component_name, 'Component: ' + data.component_name); // Component name exists
     start();
   });
